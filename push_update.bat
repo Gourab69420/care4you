dfb7473cb6f9432185116e17ba573110314bb40b
Gourab69420 <144847064+Gourab69420@users.noreply.github.com>
2026-01-12 14:34:35 +0530

    Delete push_update.bat

--- push_update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\goura\Music\Impectus 2\Impectus 2\Impectus"
-git add .
-git commit -m "feat: Add Railway backend URL"
-git push origin main
-echo Done! Now deploy frontend on Railway
-pause
